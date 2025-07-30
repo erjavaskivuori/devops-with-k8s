f8ad8ccbe20a517d885c7d365661b2d1b8f4b553
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-07-30 21:51:19 +0300

    Exercise 1.1.

---
+++ LogOutput/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        string id = Guid.NewGuid().ToString();
+
+        while (true)
+        {
+            string timestamp = DateTime.UtcNow.ToString("o");
+
+            Console.WriteLine($"{timestamp}: {id}");
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+}
---
+++ LogOutput/README.md
@@ -0,0 +1,7 @@
+# Log output app
+
+To deploy the application run:
+
+```bash
+kubectl apply -f manifests
+```
